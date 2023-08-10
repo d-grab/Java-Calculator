9226ab1ba9ecc57f5d64e88723350fa3471e0bf9
d-grab <danielgrabinski@yahoo.com>
2023-08-10 14:12:04 +0100

    Dividing code into classes and methods

--- src/Main.java
+++ src/Main.java
@@ -24,43 +24,6 @@ public class Main {
 
 
 
-        System.out.println("Are You sure the first number ("+num1+") and second number ("+ num2+") are correct ?");
-        System.out.println("Enter the operator (+,-,*,/):");
-
-        char op = sc.next().charAt(0);
-        double o = 0;
-
-        switch (op) {
-            // case to add two numbers
-            case '+':
-                o = num1 + num2;
-                break;
-
-            // case to subtract two numbers
-            case '-':
-                o = num1 - num2;
-                break;
-
-            // case to multiply two numbers
-            case '*':
-                o = num1 * num2;
-                break;
-
-            // case to divide two numbers
-            case '/':
-                o = num1 / num2;
-                break;
-
-            default:
-                System.out.println("You enter wrong input");
-        }
-
-        System.out.println("The final result:");
-        System.out.println();
-
-        // print the final result
-        System.out.println(num1 + " " + op + " " + num2
-                + " = " + o);
     }
 }
 
